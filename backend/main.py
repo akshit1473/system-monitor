73849699b3affc9ccecc5ded3321650beba16109
akshit1473 <akshitjha999999@gmail.com>
2026-04-24 23:25:40 +0530

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import fastAPI 
+from fastapi import FastAPI
 import subprocess
 import json
 
@@ -6,22 +6,25 @@ app = FastAPI()
 
 def run_script(script):
     result = subprocess.run(
-							["bash", script , "--json"],
-							capture_output=True,
-							text=True
-)
-if result.returncode != 0:
-   return{"status":"error", "error":result.stderr}
+        ["bash", script, "--json"],
+        capture_output=True,
+        text=True
+    )
 
-try:
-	 return json.loads(result.stdout)
-except: 
-       return {"status":"error", "raw":result.stdout}
+    if result.returncode != 0:
+        return {"status": "error", "error": result.stderr}
+
+    try:
+        return json.loads(result.stdout)
+    except:
+        return {"status": "error", "raw": result.stdout}
+
 
 @app.get("/")
 def root():
-	return {"status":"working"}
+    return {"status": "working"}
+
 
 @app.get("/api/path")
 def path():
-	return run_script("scripts/path_auditor.sh")
+    return run_script("scripts/path_auditor.sh")
