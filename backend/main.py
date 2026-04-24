353d62396330897cf11acee7ca53e795c2e6da1e
akshit1473 <akshitjha999999@gmail.com>
2026-04-24 23:17:13 +0530

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,28 @@
+from fastapi import fastAPI 
+import subprocess
+import json
 
+app = FastApi()
+
+def run_script(script):
+    result = subprocess.run(
+["bash", script , "--json"],
+capture_output=True,
+text=True
+)
+
+ if result.returncode != 0:
+   return{"status":"error", "error":result.stderr}
+
+try:
+     return json.loads(result.stdout)
+except: 
+       return {"status":"error", "raw":result.stdout}
+
+@app.get("/")
+def root():
+	   return {"status":"working"}
+
+@app.get("/api/path")
+def path():
+           return run_script("scripts/path_auditor.sh")
